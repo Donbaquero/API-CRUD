fdc59af271e6b5c333728d7f5780a2ee83c3380f
Donbaquero <andresbaquero99@hotmail.com>
2024-07-19 19:32:40 -0500

    Configuración inicial para servidor - Configurar el servidor Express con TypeScript - Se agregó conexión MongoDB usando mongoose. - configurado Handlebars como motor de visualización. - Configurar enrutamiento básico y estructura de vistas

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,49 @@
 import express from 'express';
+import morgan from 'morgan';
+import { engine } from 'express-handlebars';
+import path from 'path';
 
-class application{
+//Routes
+import indexRoutes from './routes/index';
+
+class Application {
     app: express.Application;
-    
-    constructor(){
+
+    constructor() {
         this.app = express();
+        this.settings();
+        this.middlewares();
+        this.routes();
     }
 
-    start(){
-        this.app.listen(3000, () => {
-            console.log('server runnig');
+    settings() {
+        this.app.set('port', 3000);
+        this.app.set('views', path.join(__dirname, 'views'));
+        this.app.engine('.hbs', engine({
+            extname: '.hbs',
+            defaultLayout: 'main',
+            layoutsDir: path.join(this.app.get('views'), 'layouts'),
+            partialsDir: path.join(this.app.get('views'), 'partials')
+        }));
+        this.app.set('view engine', '.hbs');
+    }
+
+    middlewares() {
+        this.app.use(morgan('dev'));
+        this.app.use(express.json());
+    }
+
+    routes() {
+        this.app.use('/',indexRoutes);
+
+        this.app
+    }
+
+    start() {
+        this.app.listen(this.app.get('port'), () => {
+            console.log('server on port', this.app.get('port'));
         });
     }
 }
 
-export default application; 
+export default Application;
